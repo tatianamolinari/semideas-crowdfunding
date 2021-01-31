8da0323651de51ed378dece302753c34f4859389
tatianamolinari <tatianamolinari90@gmail.com>
2021-01-31 19:19:54 -0300

    Contrato creado testeado. Sigo con error de limite de gas con ganche, no se soluciona aumentando el límite de gas en Ganache, ni cambiando la línea. Si corro de a un archivo, funciona regio.

--- test/CrowdFundingCampaing.test.js
+++ test/CrowdFundingCampaing.test.js
@@ -14,34 +14,29 @@ const expect = chai.expect;
 
 contract("CrowdFundingCampaing Test", async accounts => {
 
+    const [authorAddress, memberAccount, anotherAccount] = accounts;
     beforeEach(async() => {
-        let instanceCRUD = await CrowdFundingCampaingCRUD.deployed();
-        const tx = await instanceCRUD.createCrowdFundingCampaing("Nombre campaña", 5, 500);
-        const { logs } = tx;
-        const log = logs[0];
-        let campaingAdress = log.args._campaingAddress;
-        console.log("Address campaing: " + campaingAdress);
-        this.campaing = await CrowdFundingCampaing.at(campaingAdress);
+        //let instanceCRUD = await CrowdFundingCampaingCRUD.deployed();
+        //const tx = await instanceCRUD.createCrowdFundingCampaing("Test campaña", 5, 500);
+        //const { logs } = tx;
+        //const log = logs[0];
+        //let campaingAdress = log.args._campaingAddress;
+        //console.log("Address campaing: " + campaingAdress);
+        this.campaing = await CrowdFundingCampaing.new("Test campaña", 5, 500, authorAddress) //.at(campaingAdress);
 
     })
-
-    const [authorAddress, memberAccount, anotherAccount] = accounts;
 
     it("Checking CrowdFunding Campaing values from scratch", async() => {
         let campaing = this.campaing
 
         expect(campaing).to.be.instanceof(CrowdFundingCampaing);
 
-        expect(campaing.name()).to.eventually.be.equal("Nombre campaña");
-        expect(campaing.minimunContribution()).to.eventually.be.a.bignumber.equal(5);
-        expect(campaing.goal()).to.eventually.be.equal(500);
-        expect(campaing.active()).to.eventually.be.equal(true);
+        expect(campaing.name()).to.eventually.be.equal("Test campaña");
+        expect(campaing.minimunContribution()).to.eventually.be.equal(new BN(5));
+        expect(campaing.goal()).to.eventually.be.equal(new BN(500));
+        //expect(campaing.active()).to.eventually.be.equal(true);
 
-        expect(campaing.proposals()).to.eventually.be.instanceof(Array);
-        expect(campaing.proposals()).to.eventually.have.property('length', 0);
-
-        expect(campaing.members()).to.eventually.be.instanceof(Array);
-        expect(campaing.members()).to.eventually.have.property('length', 1);
+        expect(campaing.getProposalsCount()).to.eventually.have.property('length', 0);
         expect(campaing.membersCount()).to.eventually.be.equal(0);
 
 
--- truffle-config.js
+++ truffle-config.js
@@ -7,7 +7,7 @@ module.exports = {
     networks: {
         development: {
             port: 7545,
-            network_id: "*",
+            network_id: "5777",
             host: "127.0.0.1"
         }
     },

a78629c2bcf8348b2ee19ffbb2f17e210dbffc18
tatianamolinari <tatianamolinari90@gmail.com>
2021-05-18 22:11:01 -0300

    Agregando botones a la vista y tratando de agregar modal

--- client/src/components/DisplayContent.js
+++ client/src/components/DisplayContent.js
@@ -5,22 +5,27 @@ import { fromStatusToBadgeClass } from "../utils/utils.js"
 import ImagesDetail from "./ImagesDetail.js";
 
 class DisplayContent extends React.Component {
+
   render() {
     const badge_status = fromStatusToBadgeClass(this.props.data.status);
-
+    const a = false;
     const images = ["/images/prueba/prueba1.jpg","/images/prueba/prueba2.jpg","/images/prueba/prueba3.jpg",,"/images/prueba/prueba4.jpg"]
     const descripcion = "Esta descripcion es mockeada porque todavía no agregamos el resto de los datos a IPFS. \n La idea es que se vea de una manera linda, ordenada, y que si es largo se pueda hacer scroll. Por ahora esta esto para mostrar y pongo fotos de gatitos porque me encantan."
 
     return (<Col className="display-content" lg={9}>
               <div className="campaign-info">            
-                <h3 className="name"> Titulo de la campaña </h3>
+                <h3 className="name"> Este titulo está mockeado </h3>
                 <ImagesDetail images={images}/>
                 <h5> Estado: <Badge variant={badge_status}> { this.props.data.status } </Badge> </h5>
                 <p className="description"> {descripcion} </p>
+                <hr/>
+                <div> Para que esta campaña comience se deben recaudar { this.props.data.goal } wei o más. </div>
                 <div> La contribución mínima es de { this.props.data.minimunContribution } wei</div> 
-                <div> Autor: { this.props.data.owner } </div>  
-                <div> Para que esta campaña comience se deben recaudar { this.props.data.goal } wei o más. </div> 
+                <div> Autor: { this.props.data.owner } </div>   
                 <div> Cantidad de miembros contribuyentes: { this.props.data.membersCount } </div>
+                <div className="contribute-row">
+                    <button class="normal-button">Quiero contribuir</button>
+                </div>
               </div>  
             </Col>);
   }

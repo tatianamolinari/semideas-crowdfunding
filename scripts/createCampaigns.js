893fa5a39c38872999905ca8b20f8ce718969f8f
tatianamolinari <tatianamolinari90@gmail.com>
2021-06-13 12:13:07 -0300

    intentos por utilizar ipfs-http-client para subir info en la creación de campañas

--- scripts/createCampaigns.js
+++ scripts/createCampaigns.js
@@ -2,11 +2,22 @@
 
 
 const CrowdFundingCampaing = artifacts.require("CrowdFundingCampaign.sol");
-//const { create } = require('ipfs-http-client');
+//import { create }  from '../node_modules/ipfs-http-client'; //-------------> falla porque no le gusta el import
+const { create } = require('ipfs-http-client') //---------> falla porque no encuentra el modulo
+//const { create } = require('../node_modules/ipfs-http-client'); //---> encuentra el modulo pero falla dentro de ipfs
 
+console.log(create);
 async function main() {
     const campaignAddressesData = {};
     campaignAddressesData["campaigns"] = [];
+
+    const json_value = {}
+    json_value["holi"] = "llegueeeeeeeeeeee"
+
+    const ipfs_client = create('https://ipfs.infura.io:5001');
+    const result = await ipfs_client.add(JSON.stringify(json_value));
+    console.log(result.path);
+
     const ipfsHash = "0x7465737415000400000000000000000000000000000000000000000000000000"
     const response = await CrowdFundingCampaing.new(10, 305, ipfsHash);
     campaignAddressesData["campaigns"].push(response.address);
